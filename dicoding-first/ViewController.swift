41db053473686cac8159a555b6566ab2a1d0d543
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-07-11 21:06:35 +0700

    initilize

--- dicoding-first/ViewController.swift
+++ dicoding-first/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var followedButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCircleImageView()
     }
-
+    
+    private var isFollow = false
+    @IBAction func setFollowed(_ sender: UIButton) {
+        isFollow = !isFollow
+        followedButton.setTitle(isFollow ? "Following" : "Follow", for: .normal)
+    }
+    
+    private func setupCircleImageView(){
+        profileImageView.layer.borderWidth = 1
+        profileImageView.layer.masksToBounds = false
+        profileImageView.layer.borderColor = UIColor.black.cgColor
+        profileImageView.layer.cornerRadius = profileImageView.frame.height/2
+        profileImageView.clipsToBounds = true
+    }
+    
 
 }
 
